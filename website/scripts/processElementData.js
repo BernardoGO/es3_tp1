f6b0bd3338715a9209c928897fe5bcd6f861d451
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2017-03-29 15:07:12 -0300

    website + novos papeis

---
+++ website/scripts/processElementData.js
@@ -0,0 +1,67 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_9t9rkRQXEeeLYdBIzSDHVQ,_RNKOMRQjEeeLYdBIzSDHVQ,_8YsnIhQqEee3kaZl503Q2AActivity"]="./../../TP01/deliveryprocesses/resources/Alcancar_Missao_Aceitavel_C893A618_3d65ef00_Activity.jpeg"
+contentPage.processPage.imageFiles["_9t9rkRQXEeeLYdBIzSDHVQ,_RNKOMRQjEeeLYdBIzSDHVQ,_8YsnIhQqEee3kaZl503Q2AActivityDetail"]="./../../TP01/deliveryprocesses/resources/Alcancar_Missao_Aceitavel_C893A618_3d65ef00_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_9t9rkRQXEeeLYdBIzSDHVQ,_RNKOMRQjEeeLYdBIzSDHVQ,_8YsnIhQqEee3kaZl503Q2AWPDependency"]="./../../TP01/deliveryprocesses/resources/Alcancar_Missao_Aceitavel_C893A618_3d65ef00_WPDependency.jpeg"
+contentPage.processPage.imageFiles["_9t9rkRQXEeeLYdBIzSDHVQ,_RNKOMRQjEeeLYdBIzSDHVQActivity"]="./../../TP01/deliveryprocesses/resources/Testes_166EFA6E_c1633694_Activity.jpeg"
+contentPage.processPage.imageFiles["_9t9rkRQXEeeLYdBIzSDHVQActivity"]="./../../TP01/deliveryprocesses/resources/Processo_5EA983D2_5ea983d2_Activity.jpeg"
+contentPage.processPage.imageFiles["_9t9rkRQXEeeLYdBIzSDHVQ,_RNKOMRQjEeeLYdBIzSDHVQ,_aNgNURQjEeeLYdBIzSDHVQActivity"]="./../../TP01/deliveryprocesses/resources/Validar_a_Estabilidade_da_Build_A4DD0B5A_19af5442_Activity.jpeg"
+contentPage.processPage.imageFiles["_9t9rkRQXEeeLYdBIzSDHVQ,_RNKOMRQjEeeLYdBIzSDHVQ,_aNgNURQjEeeLYdBIzSDHVQActivityDetail"]="./../../TP01/deliveryprocesses/resources/Validar_a_Estabilidade_da_Build_A4DD0B5A_19af5442_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_9t9rkRQXEeeLYdBIzSDHVQ,_RNKOMRQjEeeLYdBIzSDHVQ,_aNgNURQjEeeLYdBIzSDHVQWPDependency"]="./../../TP01/deliveryprocesses/resources/Validar_a_Estabilidade_da_Build_A4DD0B5A_19af5442_WPDependency.jpeg"
+contentPage.processPage.imageFiles["_9t9rkRQXEeeLYdBIzSDHVQ,_RNKOMRQjEeeLYdBIzSDHVQ,_U7PCcRQjEeeLYdBIzSDHVQActivity"]="./../../TP01/deliveryprocesses/resources/Definir_Missao_de_Avaliacao_4EF8252D_c3ca6e15_Activity.jpeg"
+contentPage.processPage.imageFiles["_9t9rkRQXEeeLYdBIzSDHVQ,_RNKOMRQjEeeLYdBIzSDHVQ,_U7PCcRQjEeeLYdBIzSDHVQActivityDetail"]="./../../TP01/deliveryprocesses/resources/Definir_Missao_de_Avaliacao_4EF8252D_c3ca6e15_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_9t9rkRQXEeeLYdBIzSDHVQ,_RNKOMRQjEeeLYdBIzSDHVQ,_U7PCcRQjEeeLYdBIzSDHVQWPDependency"]="./../../TP01/deliveryprocesses/resources/Definir_Missao_de_Avaliacao_4EF8252D_c3ca6e15_WPDependency.jpeg"
+contentPage.processPage.imageFiles["_9t9rkRQXEeeLYdBIzSDHVQ,_RNKOMRQjEeeLYdBIzSDHVQ,_b93-wRQjEeeLYdBIzSDHVQActivity"]="./../../TP01/deliveryprocesses/resources/Testar_e_Avaliar_AE608555_2332ce3d_Activity.jpeg"
+contentPage.processPage.imageFiles["_9t9rkRQXEeeLYdBIzSDHVQ,_RNKOMRQjEeeLYdBIzSDHVQ,_b93-wRQjEeeLYdBIzSDHVQActivityDetail"]="./../../TP01/deliveryprocesses/resources/Testar_e_Avaliar_AE608555_2332ce3d_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_9t9rkRQXEeeLYdBIzSDHVQ,_RNKOMRQjEeeLYdBIzSDHVQ,_b93-wRQjEeeLYdBIzSDHVQWPDependency"]="./../../TP01/deliveryprocesses/resources/Testar_e_Avaliar_AE608555_2332ce3d_WPDependency.jpeg"
+contentPage.processPage.elementUrls["_A5VysBQpEee3kaZl503Q2A"] = ["Desenvolvedor 1","TP01/roles/desenvolvedor_1_8B2E666.html"];
+contentPage.processPage.elementUrls["_8YtOMBQqEee3kaZl503Q2A"] = ["Analista","TP01/roles/analista_4C09CF89.html"];
+contentPage.processPage.elementUrls["_Em9wARSiEee3kaZl503Q2A"] = ["Analista","TP01/roles/analista_4C09CF89.html"];
+contentPage.processPage.elementUrls["_XSsH8RSlEee3kaZl503Q2A"] = ["desenvolvedor 2","TP01/roles/desenvolvedor_2_885D6FA9.html"];
+contentPage.processPage.elementUrls["_Dkph8xSjEee3kaZl503Q2A"] = ["patch notes","TP01/workproducts/patch_notes_24E05952.html"];
+contentPage.processPage.elementUrls["_-DcHIRQpEee3kaZl503Q2A"] = ["Desenvolvedor 1","TP01/roles/desenvolvedor_1_8B2E666.html"];
+contentPage.processPage.elementUrls["_8YsnIRQqEee3kaZl503Q2A"] = ["Determinar os resultados do teste","TP01/tasks/determinar_os_resultados_do_teste_3689383B.html"];
+contentPage.processPage.elementUrls["_xIwuExSlEee3kaZl503Q2A"] = ["Estagiario Testador","TP01/roles/estagiario_testador_5420F6A8.html"];
+contentPage.processPage.elementUrls["_aNgNURQjEeeLYdBIzSDHVQ"] = ["Validar_a_Estabilidade_da_Build","TP01/deliveryprocesses/Validar_a_Estabilidade_da_Build_A4DD0B5A.html"];
+contentPage.processPage.elementUrls["_8FGyMBQpEee3kaZl503Q2A"] = ["Implementar Teste","TP01/tasks/implementar_teste_120F1686.html"];
+contentPage.processPage.elementUrls["_xIwuERSlEee3kaZl503Q2A"] = ["Desenvolvedor 1","TP01/roles/desenvolvedor_1_8B2E666.html"];
+contentPage.processPage.elementUrls["_AXNWAxQpEee3kaZl503Q2A"] = ["Estagiario Testador","TP01/roles/estagiario_testador_5420F6A8.html"];
+contentPage.processPage.elementUrls["_xIwuEBSlEee3kaZl503Q2A"] = ["Analista","TP01/roles/analista_4C09CF89.html"];
+contentPage.processPage.elementUrls["_RNKOMRQjEeeLYdBIzSDHVQ"] = ["Testes","TP01/deliveryprocesses/Testes_166EFA6E.html"];
+contentPage.processPage.elementUrls["_AXNWABQpEee3kaZl503Q2A"] = ["Implementar Teste","TP01/tasks/implementar_teste_120F1686.html"];
+contentPage.processPage.elementUrls["_8FHZQBQpEee3kaZl503Q2A"] = ["Estagiario Testador","TP01/roles/estagiario_testador_5420F6A8.html"];
+contentPage.processPage.elementUrls["_8YsnIhQqEee3kaZl503Q2A"] = ["Alcancar_Missao_Aceitavel","TP01/deliveryprocesses/Alcancar_Missao_Aceitavel_C893A618.html"];
+contentPage.processPage.elementUrls["_AXNWARQpEee3kaZl503Q2A"] = ["Analista","TP01/roles/analista_4C09CF89.html"];
+contentPage.processPage.elementUrls["_DkxdwRSjEee3kaZl503Q2A"] = ["Pedidos de Mudança","TP01/workproducts/pedidos_de_mudana_F9EC5B36.html"];
+contentPage.processPage.elementUrls["_89pjwBQpEee3kaZl503Q2A"] = ["Determinar Resultados do Teste ","TP01/tasks/determinar_resultados_do_teste__2CAC45DF.html"];
+contentPage.processPage.elementUrls["_XSkzMRSlEee3kaZl503Q2A"] = ["Lista de Motivos de Teste","TP01/workproducts/lista_de_motivos_de_teste_C0496310.html"];
+contentPage.processPage.elementUrls["_XS7YgRSlEee3kaZl503Q2A"] = ["Lista de Motivos de Teste","TP01/workproducts/lista_de_motivos_de_teste_C0496310.html"];
+contentPage.processPage.elementUrls["_9t9rkRQXEeeLYdBIzSDHVQ"] = ["teste","TP01/deliveryprocesses/Processo_5EA983D2.html"];
+contentPage.processPage.elementUrls["_XSy1oRSlEee3kaZl503Q2A"] = ["Lista de Motivos de Teste","TP01/workproducts/lista_de_motivos_de_teste_C0496310.html"];
+contentPage.processPage.elementUrls["_89pjwRQpEee3kaZl503Q2A"] = ["Plano de Iteração","TP01/workproducts/plano_de_iterao_F2BA35D1.html"];
+contentPage.processPage.elementUrls["_NH3gcBSqEee3kaZl503Q2A"] = ["Analista","TP01/roles/analista_4C09CF89.html"];
+contentPage.processPage.elementUrls["_khvjMBSlEee3kaZl503Q2A"] = ["validacao_grupo","TP01/deliveryprocesses/validacao_grupo_ED556F3C.html"];
+contentPage.processPage.elementUrls["_DkxdxBSjEee3kaZl503Q2A"] = ["patch notes","TP01/workproducts/patch_notes_24E05952.html"];
+contentPage.processPage.elementUrls["_9nv7khQnEee3kaZl503Q2A"] = ["Plano de Iteração","TP01/workproducts/plano_de_iterao_F2BA35D1.html"];
+contentPage.processPage.elementUrls["_-DcHIBQpEee3kaZl503Q2A"] = ["Definir elementos de testabilidade","TP01/tasks/definir_elementos_de_testabilidade_929743A1.html"];
+contentPage.processPage.elementUrls["_xIwuEhSlEee3kaZl503Q2A"] = ["desenvolvedor 2","TP01/roles/desenvolvedor_2_885D6FA9.html"];
+contentPage.processPage.elementUrls["_A5VLoBQpEee3kaZl503Q2A"] = ["Analisar falha do sistema","TP01/tasks/analisar_falha_do_sistema_32F368A6.html"];
+contentPage.processPage.elementUrls["_8FGyMRQpEee3kaZl503Q2A"] = ["Analista","TP01/roles/analista_4C09CF89.html"];
+contentPage.processPage.elementUrls["_U7PCcRQjEeeLYdBIzSDHVQ"] = ["Definir_Missao_de_Avaliacao","TP01/deliveryprocesses/Definir_Missao_de_Avaliacao_4EF8252D.html"];
+contentPage.processPage.elementUrls["_XSqSwRSlEee3kaZl503Q2A"] = ["Lista de Motivos de Teste","TP01/workproducts/lista_de_motivos_de_teste_C0496310.html"];
+contentPage.processPage.elementUrls["_Kym5QBSqEee3kaZl503Q2A"] = ["definir avaliacao grupo","TP01/deliveryprocesses/definir avaliacao grupo_D603E62.html"];
+contentPage.processPage.elementUrls["_Dkph8BSjEee3kaZl503Q2A"] = ["Pedidos de Mudança","TP01/workproducts/pedidos_de_mudana_F9EC5B36.html"];
+contentPage.processPage.elementUrls["_b93-wRQjEeeLYdBIzSDHVQ"] = ["Testar_e_Avaliar","TP01/deliveryprocesses/Testar_e_Avaliar_AE608555.html"];
+contentPage.processPage.elementUrls["_Dk7OwRSjEee3kaZl503Q2A"] = ["Pedidos de Mudança","TP01/workproducts/pedidos_de_mudana_F9EC5B36.html"];
+contentPage.processPage.elementUrls["_9nvUgBQnEee3kaZl503Q2A"] = ["Identificar objetos do teste","TP01/tasks/identificar_objetos_do_teste_B8D86520.html"];
